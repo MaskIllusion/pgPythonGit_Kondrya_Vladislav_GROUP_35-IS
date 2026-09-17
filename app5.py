27b325df2270e3d2cac987161e67be2fc9f52071
MaskIllusion <1serial.experimens.lain1@gmail.com>
2026-09-17 09:55:49 +0300

    Rename screenshots to English names

---
+++ app5.py
@@ -0,0 +1,20 @@
+import psycopg
+
+conn = psycopg.connect(
+    host="localhost",
+    dbname="tracks_bd",
+    user="postgres",
+    password="admin"
+)
+
+cur = conn.cursor()
+
+cur.execute("SELECT Название, Год FROM tracks WHERE Год > 2015;")
+
+rows = cur.fetchall()
+
+for row in rows:
+    print(row)
+
+cur.close()
+conn.close()
