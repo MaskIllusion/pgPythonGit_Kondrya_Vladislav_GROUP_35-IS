3d8921b87bd4d14c35c04e11c5c30002e452e442
MaskIllusion <1serial.experimens.lain1@gmail.com>
2026-09-17 09:41:48 +0300

    Add Python WHERE and ORDER BY requests to PostgreSQL

---
+++ app4.py
@@ -0,0 +1,42 @@
+import psycopg
+
+conn = psycopg.connect(
+    host="localhost",
+    dbname="tracks_bd",
+    user="postgres",
+    password="admin"
+)
+
+cur = conn.cursor()
+
+cur.execute("SELECT * FROM tracks WHERE Исполнитель = 'Linkin Park';")
+
+#cur.execute("SELECT * FROM tracks WHERE Год = 2017;")
+
+#cur.execute("SELECT * FROM tracks WHERE Год < 2000;")
+
+#cur.execute("SELECT * FROM tracks WHERE Год > 2015;")
+
+#cur.execute("SELECT * FROM tracks WHERE Длительность > 200;")
+
+
+#cur.execute("SELECT * FROM tracks WHERE Длительность > 300;")
+
+#cur.execute("SELECT * FROM tracks WHERE Длительность < 170;")
+
+
+#cur.execute("SELECT * FROM tracks WHERE Длительность >= 200;")
+
+#cur.execute("SELECT * FROM tracks WHERE Исполнитель = 'Король и Шут';")
+
+
+#cur.execute("SELECT * FROM tracks ORDER BY Название ASC;") 
+
+
+rows = cur.fetchall()
+
+for row in rows:
+    print(row)
+
+cur.close()
+conn.close()
